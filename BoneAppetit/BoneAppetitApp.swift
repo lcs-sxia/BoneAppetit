6cddc88bd68391f072441e8de0de2f20a8c044d5
Steven Xia <sxia@lcs.on.ca>
2026-02-27 08:49:31 -0500

    Initial Commit

---
+++ BoneAppetit/BoneAppetitApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoneAppetitApp.swift
+//  BoneAppetit
+//
+//  Created by Steven Xia on 2026/2/27.
+//
+
+import SwiftUI
+
+@main
+struct BoneAppetitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BoneAppetit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BoneAppetit
+//
+//  Created by Steven Xia on 2026/2/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
